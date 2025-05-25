8565d3a837d5fcfa06356b655315090f59858e3b
Rohit Sharma <rohits8731@gmail.com>
2025-05-25 13:08:46 +0530

    Update

--- BE/Routes/Patient/Edit.js
+++ BE/Routes/Patient/Edit.js
@@ -1,10 +1,10 @@
 import { Router } from 'express';
-import { editInfo, editPassword } from '../../Controllers/Patient/Edit.js';
+import patientController from '../../Controllers/Patient/Edit.js';
 import { tokenAuthentication } from "../../middleware/User/Authentication.js"
 
 const router = Router();
 
-router.put('/info', tokenAuthentication, editInfo);
-router.put('/password', tokenAuthentication, editPassword);
+router.put('/info', tokenAuthentication, patientController.editInfo);
+router.put('/password', tokenAuthentication, patientController.editPassword);
 
 export default router;
