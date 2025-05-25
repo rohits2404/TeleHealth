7f63872f40c862d226647ef88ad267b18a02dd13
Rohit Sharma <rohits8731@gmail.com>
2025-05-25 12:01:00 +0530

    Updated

--- BE/Routes/Patient/Appointment/SubmitReview.js
+++ BE/Routes/Patient/Appointment/SubmitReview.js
@@ -1,7 +1,5 @@
 import { Router } from 'express';
 import { SubmitReview } from '../../../Controllers/Patient/Appointment/SubmitReview.js';
-
-import { tokenAuthentication } from '../../../Middleware/User/Authentication.js';
 const router = Router();
 
 
