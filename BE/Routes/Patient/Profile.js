c35772fe3ce79025e825359dac59db5be6e76f64
Rohit Sharma <rohits8731@gmail.com>
2025-05-25 11:36:59 +0530

    Update Authetntication.js

--- BE/Routes/Patient/Profile.js
+++ BE/Routes/Patient/Profile.js
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import { patientInfo, patientAppointments, patientDoctors, patientReviews, patientViewRequests } from '../../Controllers/Patient/Profile.js';
-import { tokenAuthentication } from '../../Middleware/User/Authentication.js';
+import { tokenAuthentication } from '../../middleware/User/Authentication.js';
 
 const router = Router();
 
